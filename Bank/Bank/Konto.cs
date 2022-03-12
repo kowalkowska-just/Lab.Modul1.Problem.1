172941a980ed2856484fd993a50aef599e246e09
kowalkowska-just <kowalkowska.just@gmail.com>
2022-03-12 13:17:11 +0100

    Bank class functions testing.

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Bank
 {
+
+    class Osoba
+    {
+        public string Imie;
+        public string Nazwisko;
+    }
+
     internal class Konto
     {
+        public Osoba Wlasciciel;
+        private decimal saldo = 0;
+        private int pin = 0;
+
+        private bool SprawdzPin(int pin)
+        {
+            if (this.pin == pin) return true;
+            return false;
+        }
+
+        public void DokonajWplaty(int pin, decimal kwota)
+        {
+            if (SprawdzPin(pin))
+            {
+                if (kwota < 0) throw new ArgumentException("Wpłacona kwota nie może być mniejsza od zera.");
+                saldo += kwota;
+            }
+            else Console.WriteLine("Podałeś nieprawidłowy PIN!");
+        }
+
+        public string DokonajWyplaty(int pin, decimal kwota)
+        {
+            if (!SprawdzPin(pin) || saldo < kwota) return "Operacja anulowana.";
+            saldo -= kwota;
+            return "Operacja zakończona pomyślnie.";
+        }
+
+        public bool ZmienPin(int nowy, int stary)
+        {
+            if (SprawdzPin(stary))
+            {
+                pin = nowy;
+                return true;
+            }
+            return false;
+        }
+
+        public string PobierzInformacje(int pin)
+        {
+            if (SprawdzPin(pin)) return string.Format("Pan/Pani {0} {1} posiada na koncie {2} zł.", Wlasciciel.Imie, Wlasciciel.Nazwisko, saldo);
+            return "Brak dostępu.";
+        }
     }
 }
--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * PROBLEM 1 - Twoja firma opracowuje program dla banku. Twoim zadaniem jest stworzenie i przetestowanie klasy Konto.
+ * Klasa Konto posiada następujące pola: Wlasciciel (typu Osoba (Imie, Nazwisko)), saldo (typu decimal), pin (typu int). 
+ * Pola saldo i pin powinny być zainicjalizowane wartością zero. Pole pin można zmienić tylko podając obecną wartość pola. 
+ * Wypłatę z konta (zminiejszenie wartości pola saldo) można uzyskać tylko po podaniu prawidłowego PIN-u. Na koncie nie można zrobić debetu. 
+ * Dodaj metodę, która zwraca informacje o koncie, oczywiście pod warunkiem, że został podany prawidłowy PIN. 
+ */
+
 namespace Bank
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Konto[] konto = new Konto[2];
+
+            konto[0] = new Konto();
+            konto[0].Wlasciciel = new Osoba();
+            konto[0].Wlasciciel.Imie = "Justyna";
+            konto[0].Wlasciciel.Nazwisko = "Kowalkowska";
+
+            konto[1] = new Konto();
+            konto[1].Wlasciciel = new Osoba();
+            konto[1].Wlasciciel.Imie = "Jan";
+            konto[1].Wlasciciel.Nazwisko = "Kowalski";
+
+            Console.WriteLine("Próba zmiany pinu: {0}", konto[0].ZmienPin(1111, 0));
+            Console.WriteLine("Próba zmiany pinu: {0}", konto[1].ZmienPin(1111, 1111));
+
+            Console.WriteLine("Dokonujemy wpłaty:");
+            konto[0].DokonajWplaty(1111,1200);
+            konto[1].DokonajWplaty(0,2000);
+
+            Console.WriteLine("Dokonujemy wypłaty: {0}", konto[0].DokonajWyplaty(1111, 300));
+            Console.WriteLine("Dokonujemy wypłaty: {0}", konto[0].DokonajWyplaty(1111, 3000));
+            Console.WriteLine("Dokonujemy wypłaty: {0}", konto[1].DokonajWyplaty(1111, 200));
+
+            Console.WriteLine("Informacje o koncie: {0}", konto[0].PobierzInformacje(1111));
+            Console.WriteLine("Informacje o koncie: {0}", konto[1].PobierzInformacje(1111));
+            Console.WriteLine("Informacje o koncie: {0}", konto[1].PobierzInformacje(0));
+
+            Console.ReadKey();
         }
     }
 }
